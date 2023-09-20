3882118323c9c8c7ee31ebe48acf0f3073e66ff5
JollyRoger5150 <142903685+JollyRoger5150@users.noreply.github.com>
2023-09-21 01:10:15 +0700

    Delete main.js

--- main.js
+++
@@ -1,4 +0,0 @@
-const yourAge = 20
-const age18 = 18
-
-yourAge === '18' ? console.log ("Вам есть 18 лет") : yourAge < '18' ? console.log ("Вам меньше 18-ти лет") : yourAge > '18' ? console.log ("Вам больше 18-ти лет") : console.log ("Введите ваш возраст")
