cacee4488aea66c26868ff35e2d6b498b330d569
JollyRoger5150 <142903685+JollyRoger5150@users.noreply.github.com>
2023-09-20 18:03:32 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+const yourAge = 20
+const age18 = 18
+
+yourAge === '18' ? console.log ("Вам есть 18 лет") : yourAge < '18' ? console.log ("Вам меньше 18-ти лет") : yourAge > '18' ? console.log ("Вам больше 18-ти лет") : console.log ("Введите ваш возраст")
